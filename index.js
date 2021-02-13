6fc9381ceea1cf502dfe54f98cdb618dde52fa98
Stalin Maza <stalinct97@gmail.com>
2021-02-12 22:17:11 -0500

    add cors module to app

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const { port } = require('./config');
 let routes = require('./routes');
+const cors = require('cors');
 
 app.use('/api', routes);
+//enable cors
+app.use(cors());
+app.options('*', cors());
 app.listen(port);
 
 console.log(`API is running on http://localhost:${port}`);

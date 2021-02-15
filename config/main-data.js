a89177808a53ac5ebb618885f0c7de791daff688
Stalin Maza <stalinct97@gmail.com>
2021-02-14 20:16:46 -0500

    mejora en los datos para incluir lat-lng provincias

---
+++ config/main-data.js
@@ -0,0 +1,156 @@
+const provinces = [
+    {
+        "id": 1,
+        "provincia": "Galápagos",
+        "latitude": -0.59325112,
+        "longitude": -90.72509766
+    },
+    {
+        "id": 4,
+        "provincia": "Esmeraldas",
+        "latitude": 0.77451254,
+        "longitude": -79.41467285
+    },
+    {
+        "id": 5,
+        "provincia": "Manabí",
+        "latitude": -0.80746839,
+        "longitude": -80.05737305
+    },
+    {
+        "id": 2,
+        "provincia": "Guayas",
+        "latitude": -2.22966164,
+        "longitude": -79.91455078
+    },
+    {
+        "id": 6,
+        "provincia": "Santa Elena",
+        "latitude": -2.09791973,
+        "longitude": -80.57922363
+    },
+    {
+        "id": 7,
+        "provincia": "Los Ríos",
+        "latitude": -1.3402098,
+        "longitude": -79.5300293
+    },
+    {
+        "id": 8,
+        "provincia": "El Oro",
+        "latitude": -3.48600645,
+        "longitude": -79.83215332
+    },
+
+    {
+        "id": 15,
+        "provincia": "Carchi",
+        "latitude": 0.643977,
+        "longitude": -77.7575
+    },
+    {
+        "id": 11,
+        "provincia": "Imbabura",
+        "latitude": 0.34606723,
+        "longitude": -78.11828613
+    },
+    {
+        "id": 14,
+        "provincia": "Pichincha",
+        "latitude": -0.22418919,
+        "longitude": -78.50795746
+    },
+
+    {
+        "id": 3,
+        "provincia": "Sto. Domingo Tsáchilas",
+        "latitude": -0.29662953,
+        "longitude": -79.07958984
+    },
+
+    {
+        "id": 13,
+        "provincia": "Cotopaxi",
+        "latitude": -0.93791587,
+        "longitude": -78.64013672
+    },
+
+
+    {
+        "id": 9,
+        "provincia": "Bolívar",
+        "latitude": -1.59281238,
+        "longitude": -78.99169922
+    },
+
+    {
+        "id": 12,
+        "provincia": "Tungurahua",
+        "latitude": -1.24547685,
+        "longitude": -78.62503052
+    },
+    {
+        "id": 10,
+        "provincia": "Chimborazo",
+        "latitude": -1.68615793,
+        "longitude": -78.70605469
+    },
+    {
+        "id": 16,
+        "provincia": "Cañar",
+        "latitude": -2.74553072,
+        "longitude": -78.88183594
+    },
+    {
+        "id": 17,
+        "provincia": "Azuay",
+        "latitude": -2.90463882,
+        "longitude": -79.00817871
+    },
+    {
+        "id": 18,
+        "provincia": "Loja",
+        "latitude": -4.02317911,
+        "longitude": -79.21691895
+    },
+
+    {
+        "id": 19,
+        "provincia": "Sucumbíos",
+        "latitude": -0.00549316,
+        "longitude": -76.70654297
+    },
+    {
+        "id": 20,
+        "provincia": "Orellana",
+        "latitude": -0.81845361,
+        "longitude": -76.25610352
+    },
+    {
+        "id": 24,
+        "provincia": "Napo",
+        "latitude": -1.00730794,
+        "longitude": -77.8326416
+    },
+    {
+        "id": 22,
+        "provincia": "Pastaza",
+        "latitude": -1.49397131,
+        "longitude": -78.01391602
+    },
+    {
+        "id": 21,
+        "provincia": "Morona Santiago",
+        "latitude": -2.30101696,
+        "longitude": -78.1237793
+    },
+
+    {
+        "id": 23,
+        "provincia": "Zamora Chinchipe",
+        "latitude": -4.09989026,
+        "longitude": -78.95874023
+    }
+];
+
+module.exports.ProvincesData = provinces;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const request = require('request');
 const cheerio = require('cheerio');
 const { replaceAll } = require('../lib/utils.js');
+const { dateFormatConfig } = require('../config/date-config.js');
+const dateFormat = require("dateformat");
+
+const { ProvincesData } = require("../config/main-data");
+
+dateFormat.i18n = dateFormatConfig;
+const server_date = new Date();
+const dateFormatted = {
+    natural_time: dateFormat(server_date, "dddd dd mmmm yyyy, h:MM:ss TT"),
+    datetime: dateFormat(server_date, "mm-dd-yyyy HH:MM:ss")
+};
 
 // Define the index api route
 router.get('/', function (req, res) {
@@ -23,12 +34,14 @@ router.get('/', function (req, res) {
             });
             const json = {
                 code: 200,
+                requested_at: dateFormatted,
                 data,
             };
             res.send(json)
         } else {
             res.status(500).send({
                 code: 500,
+                requested_at: dateFormatted,
                 error,
             });
         }
@@ -68,17 +81,28 @@ router.get('/provinces', function (req, res) {
                 const merged = keys.reduce((obj, key, index) => ({ ...obj, [key]: values[index] }), {});
                 return merged;
             });
-            
+
             data.splice((data.length) - 1, 1);
+
+            data = data.map((province_row) => {
+                const config_province = ProvincesData.filter(province_config => province_config.provincia.toLowerCase() == province_row.provincia.toLowerCase());
+                if(config_province && config_province.length > 0){
+                    return {...province_row, ...config_province[0]};
+                }
+                return {...province_row, latitude: null, longitud: null, id: null};
+            });
 
             const json = {
                 code: 200,
+                requested_at: dateFormatted,
+                tz: process.env.TZ,
                 data,
-                total: data.length
+                total: data.length,
             };
             res.send(json)
         } else {
             res.status(500).send({
+                requested_at: dateFormatted,
                 code: 500,
                 error,
             });

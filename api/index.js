495472c8b5736408d0a73aaa37e56b11f565e995
Stalin Maza <stalinct97@gmail.com>
2021-02-14 19:13:10 -0500

    orden de configuracion cors

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ const { port } = require('../config');
 const cors = require('cors');
 let routes = require('../routes');
 
+app.use(cors());
+app.options('*', cors());
+
 app.get("/", function (req, res) {
     res.json(`Api Server works correctly on domain: ${req.hostname}`)
 });
 
 app.use('/api', routes);
-app.use(cors());
-app.options('*', cors());
 app.listen(port);
 
 console.log(`API ROUTES is running on http://localhost:${port}`);
--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 const { port } = require('./config');
 let routes = require('./routes');
 const cors = require('cors');
+//enable cors
+app.use(cors());
+app.options('*', cors());
 
 app.get("/", function (req, res) {
     res.json(`Main Server works correctly on domain: ${req.hostname}`)
 });
 
 app.use('/api', routes);
-//enable cors
-app.use(cors());
-app.options('*', cors());
 app.listen(port);
 
 console.log(`API INDEX is running on http://localhost:${port}`);
